d4a3ac699929a904781f5d17169158a14889c054
ianlucic <il058418@ohio.edu>
2021-01-27 19:53:25 -0500

    Intitial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ian Lucic>
+ *        Date: <1-27-2021>
+ * Description: Created for CS 2400 Lab 2 Section 107. 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+    return 0;
+}// main
